930f9349dd35fd1bba1b930d0c61758bd2d44a99
SubhankarPradhan88 <70726910+SubhankarPradhan88@users.noreply.github.com>
2021-03-04 04:18:42 +0530

    Updated deployment server IP
    Updated deployment server IP

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -10,7 +10,7 @@ class Controller extends Component {
   constructor()
   {
     super();
-    this.baseUrl = "http://18.207.189.220:8080/api/v1/";
+    this.baseUrl = "http://54.166.214.244:8080/api/v1/";
   }
   render(){
     return(
